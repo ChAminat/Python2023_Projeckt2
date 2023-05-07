9ee5807681547b090869331f799c3dab7a542ec3
AminatChililova <chililova.aa@phystech.edu>
2023-05-07 18:24:31 +0300

    working web-system

---
+++ main.py
@@ -0,0 +1,5 @@
+#from Interface import WorkingSpace
+from telegram_bot import TelegramBot, bot
+
+if __name__ == '__main__':  # будет возвожность войти в существующий аккаунт
+    tgbot = TelegramBot(bot = bot)
